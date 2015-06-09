752910e2022ac65205373504a027f4f3da62a1c4
unknown <Schulung@NB-1YLSHV1.CHECK24.INTERN>
2015-06-09 09:57:52 +0200

    Add first test

---
+++ Poker/Poker.Tests/Library1.fs
@@ -0,0 +1,19 @@
+﻿namespace Poker.Tests
+
+open Xunit
+
+module Tests =
+    let When_a_hand_has_no_combination_the_score_is_the_highest_card() = 
+        // Arrange
+        let hand = [
+                   {Number =2; Suit = Diamond; }
+                   {Number =3; Suit = Diamond; }
+                   {Number =6; Suit = Diamond; }
+                   {Number =7; Suit = Diamond; }
+                   {Number =9; Suit = Diamond; }
+                   ]
+        // Act
+        let result = GetHandCombination hand
+
+        // Assert
+        Xunit.Assert.Equal(HighCard, result)
