1289ffa55b061ac2b6aa8be5f4769bd9c113174c
Maximilian Beller <maximilian.beller@beller-it.de>
2015-06-09 10:34:00 +0200

    Make compilable

--- Poker/Poker.Tests/Tests.fs
+++ Poker/Poker.Tests/Tests.fs
@@ -5,17 +5,18 @@ open Schulung.Poker
 open Schulung.Poker.Poker
 
 module Tests =
+    [<Fact>]
     let When_a_hand_has_no_combination_the_score_is_the_highest_card() = 
         // Arrange
         let hand = [
-                   {Value = CardValue.Number 2; Suit = Diamonds; }
-                   {Value = CardValue.Number 3; Suit = Diamonds; }
-                   {Value = CardValue.Number 6; Suit = Diamonds; }
-                   {Value = CardValue.Number 7; Suit = Diamonds; }
-                   {Value = CardValue.Number 9; Suit = Diamonds; }
+                   {Value = CardValue.Number2; Suit = Diamonds; }
+                   {Value = CardValue.Number3; Suit = Diamonds; }
+                   {Value = CardValue.Number6; Suit = Diamonds; }
+                   {Value = CardValue.Number7; Suit = Diamonds; }
+                   {Value = CardValue.Number9; Suit = Diamonds; }
                    ]
         // Act
         let result = Poker.GetHandCombination hand
 
         // Assert
-        Xunit.Assert.Equal(HighCard, result)
+        Xunit.Assert.Equal(HandCombination.HighCard, result)
--- Poker/Poker/Poker.fs
+++ Poker/Poker/Poker.fs
@@ -8,16 +8,35 @@ module Poker =
     | Clubs
 
     type CardValue =
-    | Ace
-    | King
-    | Queen
+    | Number2
+    | Number3
+    | Number4
+    | Number5
+    | Number6
+    | Number7
+    | Number8
+    | Number9
+    | Number10
     | Jack
-    | Number of int
+    | Queen
+    | King
+    | Ace
 
     type Card = {
         Value : CardValue
         Suit : Suit
     }
 
+    type HandCombination = 
+    | HighCard
+    | OnePair
+    | TwoPair
+    | ThreeOfAKind
+    | Straight
+    | Flush
+    | FullHouse
+    | FourOfAKind
+    | StraightFlush
+    | RoyalFlush
 
-    let GetHandCombination hand = ()
+    let GetHandCombination hand = HighCard
