92865924bd9a48f277577c874fe8311814afa650
Maximilian Beller <maximilian.beller@beller-it.de>
2015-06-09 11:18:23 +0200

    Add OnePair

--- Poker/Poker.Tests/Tests.fs
+++ Poker/Poker.Tests/Tests.fs
@@ -20,3 +20,19 @@ module Tests =
 
         // Assert
         Xunit.Assert.Equal(HandCombination.HighCard, result)
+
+    [<Fact>]
+    let When_a_hand_has_one_pair_the_score_is_OnePair() = 
+        // Arrange
+        let hand = [
+                   {Value = CardValue.Number2; Suit = Diamonds; }
+                   {Value = CardValue.Number2; Suit = Diamonds; }
+                   {Value = CardValue.Number6; Suit = Diamonds; }
+                   {Value = CardValue.Number7; Suit = Diamonds; }
+                   {Value = CardValue.Number9; Suit = Diamonds; }
+                   ]
+        // Act
+        let result = Poker.GetHandCombination hand
+
+        // Assert
+        Xunit.Assert.Equal(HandCombination.OnePair, result)
--- Poker/Poker/Poker.fs
+++ Poker/Poker/Poker.fs
@@ -1,46 +1,57 @@
 ﻿namespace Schulung.Poker
 
-module Poker =
+module Poker = 
     type Suit = 
-    | Hearts
-    | Diamonds
-    | Spades
-    | Clubs
-
-    type CardValue =
-    | Number2
-    | Number3
-    | Number4
-    | Number5
-    | Number6
-    | Number7
-    | Number8
-    | Number9
-    | Number10
-    | Jack
-    | Queen
-    | King
-    | Ace
-
-    type Card = {
-        Value : CardValue
-        Suit : Suit
-    }
-
+        | Hearts
+        | Diamonds
+        | Spades
+        | Clubs
+    
+    type CardValue = 
+        | Number2
+        | Number3
+        | Number4
+        | Number5
+        | Number6
+        | Number7
+        | Number8
+        | Number9
+        | Number10
+        | Jack
+        | Queen
+        | King
+        | Ace
+    
+    type Card = 
+        { Value : CardValue
+          Suit : Suit }
+    
     type HandCombination = 
-    | HighCard
-    | OnePair
-    | TwoPair
-    | ThreeOfAKind
-    | Straight
-    | Flush
-    | FullHouse
-    | FourOfAKind
-    | StraightFlush
-    | RoyalFlush
-
+        | HighCard
+        | OnePair
+        | TwoPair
+        | ThreeOfAKind
+        | Straight
+        | Flush
+        | FullHouse
+        | FourOfAKind
+        | StraightFlush
+        | RoyalFlush
+    
     type Hand = Card list
-
+    
     type HandClassifier = Hand -> HandCombination
-
-    let GetHandCombination: HandClassifier = fun hand -> HighCard
+    
+    type CombinationCheck = Hand -> bool
+    
+    let handHasOnePair : CombinationCheck = 
+        fun hand -> 
+            (hand
+             |> Seq.groupBy (fun card -> card.Value)
+             |> Seq.exists (fun g -> Seq.length (snd g) = 2))
+    
+    let GetHandCombination : HandClassifier = 
+        fun hand -> 
+            match hand with
+            | x when handHasOnePair x -> OnePair
+            | _ -> HighCard
